5c3bf66bdba442d0bc88837ca3c03ddd2febb485
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-12-16 14:29:36 -0500

    Day 2 simple exercises

---
+++ day2.py
@@ -0,0 +1,26 @@
+first_name = 'Mindy'
+last_name = 'Ratcliff'
+full_name = 'Mindy Ratcliff'
+country = 'USA'
+city = 'Delaware'
+age = 36
+year = 2020
+is_married = True
+is_true = 1
+is_light_on = True
+taco, cat, meow = 'yes', 2, False
+
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(taco))
+print(type(cat))
+print(type(meow))
