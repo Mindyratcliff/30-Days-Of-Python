f003e963f95b0f6608dbc8f10e2208d5a2da2172
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-12-16 15:21:26 -0500

    Finished day 2 exercises

--- day2.py
+++ day2.py
@@ -24,3 +24,35 @@ print(type(is_light_on))
 print(type(taco))
 print(type(cat))
 print(type(meow))
+
+
+firstNum = len(first_name)
+lastNum = len(last_name)
+
+print(firstNum < lastNum)
+num_one = 5
+num_two = 4
+
+_total = num_one + num_two
+
+variable_diff = num_one - num_two
+
+_product = num_two * num_one
+
+_division = num_one/num_two
+
+_remainder = num_one % num_two
+
+_exp = num_one ** num_two
+
+_floor_division = num_one//num_two
+
+area_of_circle = 3.14 * (30 ** 2)
+print(area_of_circle)
+
+circum_of_circle = 2 * 3.14 * 30
+print(circum_of_circle)
+
+userName = input('What is your name')
+userCountry = input('Where do you live')
+userAge = input('What is your age')
